76fffc4c1d30b4df80fc94e5f243d906cc66a979
DGAVIRIA <daniel.gaviria@lineadirecta.com.co>
2019-11-01 15:35:30 -0500

    Commit con cambios 3:35

--- src/components/totalCounters/index.js
+++ src/components/totalCounters/index.js
@@ -3,23 +3,32 @@ import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Card from 'react-bootstrap/Card'
+import './totalStyle.css'
 
 class totalCounters extends Component {
 
-    render() {
-        return (
-          <Card>
-        <Container className="text-center">       
-            <Row>            			
+  render() {
+    return (
+     
+        <Container className="tableTotals">
+          <Col>
+          <Row>
+              <Col>0</Col>
+              <Col>0</Col>
+              <Col>0</Col>
+              <Col>0</Col>
+            </Row>
+            <Row>
               <Col>T. Entregar</Col>
               <Col>T. Tulas</Col>
               <Col>T Cajas</Col>
               <Col>T. Bolsas</Col>
             </Row>
-          </Container>
-          </Card>
-          )
-    }
+          </Col>
+        </Container>
+     
+    )
+  }
 
 
 }
