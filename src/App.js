aaa9acb0d8bb5760e2659202ded40a4451702409
DGAVIRIA <daniel.gaviria@lineadirecta.com.co>
2019-11-01 07:31:26 -0500

    Nuevos cambios DG

--- src/App.js
+++ src/App.js
@@ -1,55 +1,67 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Provider, connect} from 'react-redux';
+import { Provider, connect } from 'react-redux';
 import Counter from './counter'
+
+import Alerts from './components/alerts/index'
 import SearchOrders from './components/searchOrders/index'
 import TableOrders from './components/tableOrders/index'
+import DeliveryCategories from './components/deliveryCategories/index'
+
+
 
 class App extends Component {
-
-  
-    state = {
-        store: this.props.store,
-        globalEventDistributor: this.props.globalEventDistributor,
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            store: this.props.store,
+            globalEventDistributor: this.props.globalEventDistributor,
+            isData: false
+        }
+    }
 
     componentDidCatch(error, info) {
         console.log(error, info);
-        
+
     }
 
-    getdata(){
+    getdata() {
         fetch('http://localhost:3000')
-        .then(response => response.json())
-        .then(json => console.log(json))
-        console.log('fin');
+            .then(response => response.json())
+            .then(json => console.log(json))
+
+        this.setState({ isData: false });
     }
 
-    componentDidMount(){
-       this.getdata();
-    }   
-    
+    componentDidMount() {
+        this.getdata();
+    }
+
     render() {
+        const {store,globalEventDistributor,isData } = this.state
         return (
-            <div>
-                {this.state.store && this.state.globalEventDistributor ? 
+            <div className= "container-fluid">
+                {store && globalEventDistributor ?
                     <Provider store={this.state.store}>
-                    <div className="row">
-                        <div className="col-7" id="searchComp">
-                            <SearchOrders/>
-                            <TableOrders/>
-                            {/* <Counter globalEventDistributor={this.state.globalEventDistributor}/> */}
+                        <div className="row">
+                            <div className="col-7" id="searchComp">
+                                <SearchOrders />
+                                <TableOrders />
+                             { isData ? <Alerts /> : false }
+                                {/* <Counter globalEventDistributor={this.state.globalEventDistributor}/> */}
+                            </div>
+                            <div className="col-5">
+                                <DeliveryCategories />
+                            </div>
                         </div>
-                        <div className="col-5">
-                            <h1>holas</h1>
-                        </div>
-                    </div>
                     </Provider> :
                     <div>EL store no ha sido iniciado </div>}
             </div>
         )
-    }   
+    }
+
+
 
 
 
